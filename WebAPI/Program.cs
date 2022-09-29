6ef70c9a5fba96b46fbda75e2c2b1242ae79c913
lure110 <abromavicius.arnas@gmail.com>
2022-09-29 13:44:41 +0300

    WebApi created regions and landplots

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,10 +1,18 @@
+using webAPI.Data;
+using webAPI.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<RestContext>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddTransient<IRegionsRepository, RegionsRepository>();
+builder.Services.AddTransient<ILandplotsRepository, LandplotsRepository>();
+
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
